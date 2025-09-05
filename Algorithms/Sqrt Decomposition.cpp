2663d8ea23313db502a42edea8a4965baa612bc7
Fojla Rabbi <fojlarabbi018@gmail.com>
2025-09-05 22:04:52 +0600

    Create Sqrt Decomposition.cpp

---
+++ Algorithms/Sqrt Decomposition.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a (n);
+    for (int i=0; i<n; i++) {
+        cin >> a[i];
+    }
+
+    // preprocessing
+    int len = ceil(sqrt (n + .0)); // size of the block and the number of blocks
+    vector<int> b (len, 0);
+    for (int i=0; i<n; i++) {
+        b[i / len] += a[i];
+    }
+
+    int q;
+    cin >> q;
+    // answering the queries
+    // O(q*sqrt(n))
+    for (int p=1; p<=q; p++) {
+        int l, r;
+        cin >> l >> r;
+        int sum = 0;
+        for (int i=l; i<=r; ) {
+            if (i % len == 0 && i + len - 1 <= r) {
+                // if the whole block starting at i belongs to [l, r]
+                sum += b[i / len];
+                i += len;
+            }
+            else {
+                sum += a[i];
+                i++;
+            }
+        }
+        cout << sum <<"\n";
+    }
+    return 0;
+}
