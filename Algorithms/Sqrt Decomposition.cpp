51fc5fa283f873009bc4c89137f51bdc95b6a1e3
Fojla Rabbi <fojlarabbi018@gmail.com>
2025-11-01 13:29:43 +0600

    Update Sqrt Decomposition.cpp

--- Algorithms/Sqrt Decomposition.cpp
+++ Algorithms/Sqrt Decomposition.cpp
@@ -3,32 +3,33 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    vector<int> a (n);
-    for (int i=0; i<n; i++) {
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
         cin >> a[i];
     }
 
-    // preprocessing
-    int len = ceil(sqrt (n + .0)); // size of the block and the number of blocks
-    vector<int> b (len, 0);
-    for (int i=0; i<n; i++) {
-        b[i / len] += a[i];
+    // Preprocessing
+    int len = ceil(sqrt(n + .0)); 
+    // len is the size of the block and the number of blocks
+    vector<int> block(len, 0);
+    for(int i = 0; i < n; i++) {
+        block[i / len] += a[i];
     }
 
     int q;
     cin >> q;
-    // answering the queries
-    // Worst case for a query ---> (sqrt(n)-1) + (sqrt(n)-1) = 2*(sqrt(n)-1)
-    // (when l=0 and r=n-2)...... whatever, it's asymptotically sqrt(n)
+    // Answering the queries
+    // Worst case ---> (sqrt(n) - 1) + (sqrt(n) - 2) + (sqrt(n) - 1) = 3*sqrt(n) - 4
+    // (when l = 1 and r = n-2)...... whatever, it's asymptotically sqrt(n)
     // O(q*sqrt(n))
-    for (int p=1; p<=q; p++) {
+    for(int p = 1; p <= q; p++) {
         int l, r;
         cin >> l >> r;
         int sum = 0;
-        for (int i=l; i<=r; ) {
+        for (int i = l; i <= r; ) {
             if (i % len == 0 && i + len - 1 <= r) {
                 // if the whole block starting at i belongs to [l, r]
-                sum += b[i / len];
+                sum += block[i / len];
                 i += len;
             }
             else {
